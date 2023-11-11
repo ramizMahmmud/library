f7bf1470af1aec49a44a0d6dd3f31adbcca977ff
ramizMahmmud <89058495+ramizMahmmud@users.noreply.github.com>
2023-11-11 11:02:52 +0600

    Diplay objects
    showBooks function is added to manupulate dom
    book-card class added to css file to design book card
    a div with id book-list added inorder to append Book list.

--- main.js
+++ main.js
@@ -1,8 +1,59 @@
 const myLibrary = [];
 
+let lengthOfArry;
+
+let i = 0;
+
 function Book (name, author, page) {
     this.name = name;
     this.author = author;
     this.page = page;
 };
+
+
+function showCard(){
+    document.querySelector('.container').style.display = 'flex';
+}
+
+function hideCard(){
+    document.querySelector('.container').style.display = 'none';
+}
+function showBooks(){
+    lengthOfArry = myLibrary.length;  
+    for(i; i < lengthOfArry; i++){
+        const bookCard = document.createElement('div');
+        const bookName = document.createElement('div');
+        const bookAuthor = document.createElement('div');
+        const bookPage = document.createElement('div');
+        bookName.innerHTML = `Name: ${myLibrary[i].name}`;
+        bookAuthor.innerHTML = `Author: ${myLibrary[i].author}`;
+        bookPage.innerHTML = `Page: ${myLibrary[i].page}`;
+
+        bookCard.appendChild(bookName);
+        bookCard.appendChild(bookAuthor);
+        bookCard.appendChild(bookPage);
+        bookCard.classList.add('book-card');
+        document.getElementById('book-list').appendChild(bookCard);
+    }
+    
+}
+function addBooks(event){
+    event.preventDefault();
+    let bookTitle = document.getElementById('title').value;
+    let bookAuthor = document.getElementById('author').value;
+    let bookPage =document.getElementById('page').value;
+    let book = new Book(bookTitle, bookAuthor, bookPage);
+    myLibrary.push(book);
+
+    showBooks()
+
+    hideCard();
+}
+
+
+document.querySelector('.add').addEventListener('click',showCard);
+document.querySelector('.close-btn').addEventListener('click', hideCard);
+document.querySelector('.card-form').addEventListener('submit',addBooks);
+
+
 
